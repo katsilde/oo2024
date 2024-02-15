64edc291dab1f944c3b994745ab6c7b521550ae4
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2024-02-15 12:24:01 +0200

    Update kodune.ts

--- 01_funktsioon/kodune.ts
+++ 01_funktsioon/kodune.ts
@@ -12,7 +12,7 @@ function plaadid(plaat:number, vuuk:number, laius:number, pikkus:number):number{
     let plaadid_kokku:number= laius_plaadid*pikkus_plaadid;
 
     //väljatrükk
-    console.log("\nPikkupidi läheb "+ pikkus_plaadid+" plaati vaja");
+    console.log("Pikkupidi läheb "+ pikkus_plaadid+" plaati vaja");
     console.log("Laiupidi läheb "+ laius_plaadid+" plaati vaja");
     console.log("Vaja läheb vähemalt "+ plaadid_kokku+" plaati");
     return plaadid_kokku;
